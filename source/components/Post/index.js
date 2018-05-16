d74b69cf461d4cf4943f90b1a07b7ecf9d4c5560
Dima Nazdratenko <d.nazdratenko@lucky-labs.com>
2018-05-17 01:33:50 +0300

    lesson 2

--- source/components/Post/index.js
+++ source/components/Post/index.js
@@ -1,19 +1,20 @@
 // Core
-import React, {Component} from 'react';
-import moment from 'moment'
+import React, { Component } from 'react';
+import moment from 'moment';
 
 // Instruments
 import avatar from 'theme/assets/lisa';
+import Styles from './styles.m.css';
 
 export default class Post extends Component {
-	render() {
-		return (
-			<section>
-				<img src={avatar}/>
-				<a>Lisa Simpson</a>
-				<time>{moment().format('MMMM D h:mm:ss a')}</time>
-				<p>Howdy!</p>
-			</section>
-		);
-	}
+    render () {
+        return (
+            <section className = { Styles.post }>
+                <img src = { avatar } />
+                <a>Lisa Simpson</a>
+                <time>{moment().format('MMMM D h:mm:ss a')}</time>
+                <p>Howdy!</p>
+            </section>
+        );
+    }
 }
