c2313d698abd885233e47186effcf4ffe400acbb
Dima Nazdratenko <d.nazdratenko@lucky-labs.com>
2018-05-17 21:10:29 +0300

    add media to CSSnext

--- webpack/modules/index.js
+++ webpack/modules/index.js
@@ -42,6 +42,18 @@ export const loadPostCSS = () => ({
                         autoprefixer:     true,
                         customProperties: false,
                         applyRule:        false,
+                        customMedia:      {
+                            extensions: {
+                                '--phonePortrait':   '(width <= 414px)',
+                                '--phoneLandscape':  '(width >= 415px) and (width <= 667px)',
+                                '--tabletPortrait':  '(width >= 668px) and (width <= 768px)',
+                                '--tabletLandscape': '(width >= 769px) and (width <= 1024px)',
+                                '--desktopS':        '(width >= 1025px) and (width <= 1366px)',
+                                '--desktopM':        '(width >= 1367px) and (width <= 1680px)',
+                                '--desktopL':        '(width >= 1681px) and (width <= 1920px)',
+                                '--desktopXL':       '(width >= 1921px)',
+                            },
+                        },
                     },
                 }),
                 mqpacker(),
