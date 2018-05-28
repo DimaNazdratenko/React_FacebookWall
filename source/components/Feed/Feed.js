90fb185411a5572127e91749eaf921b735dbeba4
Dima Nazdratenko <d.nazdratenko@lucky-labs.com>
2018-05-28 13:30:04 +0300

    lesson 5

--- source/components/Feed/Feed.js
+++ source/components/Feed/Feed.js
@@ -5,18 +5,33 @@ import React, { Component } from "react";
 import StatusBar from "components/StatusBar/StatusBar";
 import Composer from "components/Composer/Composer";
 import Post from "components/Post/Post";
+import Spinner from "components/Spinner/Spinner";
 
 // Instruments
-import Styles from './styles.m.css';
+import Styles from "./styles.m.css";
 
 export default class Feed extends Component {
+    state = {
+        posts: [
+            { id: "123", comment: "Hi there!", created: 1526825076849 },
+            { id: "456", comment: "Приветик!", created: 1526825076855 }
+        ],
+        isSpinning: false,
+    };
+
     render () {
+        const { posts, isSpinning } = this.state;
+
+        const postsJSX = posts.map((post) => {
+            return <Post key = { post.id } { ...post } />;
+        });
 
         return (
             <section className = { Styles.feed }>
+                <Spinner isSpinning = { isSpinning } />
                 <StatusBar />
                 <Composer />
-                <Post />
+                {postsJSX}
             </section>
         );
     }
