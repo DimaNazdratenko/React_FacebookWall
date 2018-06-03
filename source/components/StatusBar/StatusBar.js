b49f66f599448cd047d6db7bf0b0171589b106f1
Dima Nazdratenko <d.nazdratenko@lucky-labs.com>
2018-06-03 23:15:41 +0300

    clear code into status bar

--- source/components/StatusBar/StatusBar.js
+++ source/components/StatusBar/StatusBar.js
@@ -1,18 +1,15 @@
 // Core
 import React, { Component } from "react";
-import cx from "classnames";
 
 // Components
 import { withProfile } from "components/HOC/withProfile";
 
 // Instruments
 import StylesSimple from "./stylesSimple.m.css";
-// import Styles from "./styles";
 
 @withProfile
 export default class StatusBar extends Component {
     render () {
-        // const styles = cx(StylesSimple.statusBar, Styles);
         const {
             avatar,
             currentUserFirstName,
